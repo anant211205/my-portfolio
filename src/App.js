b1a8bf472913436cfd717c380c178c06ed2e9ee3
anant211205 <anant211205@gmail.com>
2024-01-09 18:36:46 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import "../src/styles/app.css" ;
 import { Toaster } from "react-hot-toast";
-// import Toaster from "react-hot-toast";
-// import About from "./components/About";
+import About from "./components/About";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Home/>
-      {/* <About/> */}
-      <Projects/>
+      <About/>
+      <Card/>
+      {/* <Projects/> */}
       <Contact/>
       <Footer/>
       <Toaster position="top-center"/>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import "../styles/about.css"
+import "../styles/about.css";
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import Card from "./Card";
+
 const About = () => {
+  const data = [
+    {
+      id: 0,
+      description:
+        "Passionate tech enthusiast with a knack for innovation and problem-solving. Eager to connect with like-minded professionals and explore opportunities to contribute to the dynamic tech landscape. Experienced in developing React projects, showcasing expertise in frontend technologies. Thrives on tackling challenges and continually evolving in the ever-changing world of technology. Excited about future collaborations and impactful contributions.",
+    },
+    {
+      id: 0,
+      description:
+        "Embracing the challenge of solving complex computational problems through systematic understanding of data structures. Committed to achieving expertise in DSA for creating optimized, scalable, and innovative software solutions. Excited about the continuous learning journey and its transformative impact on coding proficiency. ",
+    },
+  ];
+
+
   return (
     <>
-    <div id='about'>
-        about me
-    </div>
+      <div id="about">
+        <section className="about-top" key={data.id}>
+          <span className="heading-about">About Me</span>
+          {data.map((index) => {
+            return <p className="desc">{index.description}</p>;
+          })}
+        </section>
+        {/* <section className="about-bottom"> */}
+           
+        {/* </section> */}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
